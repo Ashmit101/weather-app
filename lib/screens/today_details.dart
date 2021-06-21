b872c5f5c69d6205970f04c356ec801ed67893f8
Ashmit Rajaure <ashmit.rajaure1234@gmail.com>
2021-06-21 13:39:45 +0545

    Add a simple layout to the details page
    See: #2

--- lib/screens/today_details.dart
+++ lib/screens/today_details.dart
@@ -12,22 +12,59 @@ class CurrentDetails extends StatefulWidget {
 }
 
 class _CurrentDetailsState extends State<CurrentDetails> {
-  Weather? weather;
+  late Weather weather;
   String message;
   var jsonWeatherMap;
+
+  // TextEditingController nameController = TextEditingController();
+  // TextEditingController mainController = TextEditingController();
+  // TextEditingController descriptionController = TextEditingController();
+  // TextEditingController temperatureController = TextEditingController();
+
   _CurrentDetailsState(this.message) {
     jsonWeatherMap = jsonDecode(message);
   }
 
   @override
   Widget build(BuildContext context) {
+    final sizeX = MediaQuery.of(context).size.width;
+    final sizeY = MediaQuery.of(context).size.height;
+    weather = Weather.fromJsonMap(jsonWeatherMap);
+
+    String name = weather.name;
+    double temperature = weather.temperature['temp'];
+    String main = weather.main;
+    String description = weather.description;
+
+    TextStyle textStyle = TextStyle(fontSize: 18);
+    TextStyle tempStyle = TextStyle(fontSize: 30);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Weather'),
-      ),
-      body: Column(
-        children: [Text(message)],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(name),
+        ),
+        body: SingleChildScrollView(
+            child: Container(
+          width: sizeX,
+          height: sizeY,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(name, style: textStyle),
+              Text(
+                temperature.toString(),
+                style: tempStyle,
+              ),
+              Text(
+                main,
+                style: textStyle,
+              ),
+              Text(
+                description,
+                style: textStyle,
+              )
+            ],
+          ),
+        )));
   }
 }
