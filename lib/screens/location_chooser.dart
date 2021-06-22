8eb5376fa68817275ca1c74de3bc70ad0f2ecf22
Ashmit Rajaure <ashmit.rajaure@gmail.com>
2021-06-22 21:30:36 +0545

    Add feature to store the location chosen

--- lib/screens/location_chooser.dart
+++ lib/screens/location_chooser.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather/screens/today_details.dart';
 import '../data/data.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+TextEditingController locationTextController = TextEditingController();
 
 class LocationChooser extends StatefulWidget {
-  const LocationChooser({Key? key}) : super(key: key);
-
   @override
   _LocationChooserState createState() => _LocationChooserState();
 }
@@ -13,12 +14,12 @@ class LocationChooser extends StatefulWidget {
 class _LocationChooserState extends State<LocationChooser> {
   String weatherInfo = '';
   String city = '';
-  TextEditingController locationTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final ButtonStyle buttonStyle =
         ElevatedButton.styleFrom(textStyle: TextStyle(fontSize: 24));
+    getLocation(context);
 
     return Scaffold(
         appBar: AppBar(
@@ -39,38 +40,53 @@ class _LocationChooserState extends State<LocationChooser> {
                 ),
                 ElevatedButton(
                   style: buttonStyle,
-                  onPressed: () => printJson(context),
+                  onPressed: () => downloadWeatherJson(context),
                   child: Text('Submit'),
                 ),
-                Text(weatherInfo),
               ],
             ),
           ),
         ));
   }
-
-  printJson(BuildContext context) async {
-    //Get http response
-    city = locationTextController.text;
-    String message;
-    if (city.length > 1) {
-      var response = await http.get(Uri.parse(
-          "http://api.openweathermap.org/data/2.5/weather?q=$city&appid=${API.apiKey}"));
-      message = response.body;
-      print('Status code : ${response.statusCode}');
-      if (response.statusCode == 200) {
-        currentWeather(context, response.body);
-      }
-    } else {
-      message = 'Please enter a city name';
-    }
-    setState(() {
-      weatherInfo = message;
-    });
-  }
 }
 
-currentWeather(BuildContext context, String message) {
+void getLocation(BuildContext context) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? savedLocation = prefs.getString('location');
+  downloadWeatherJson(context, savedLocation);
+}
+
+void saveLocation(String city) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('location', city);
+}
+
+gotoCurrentWeather(BuildContext context, String message) {
   Navigator.push(context,
       MaterialPageRoute(builder: (context) => CurrentDetails(message)));
+}
+
+downloadWeatherJson(BuildContext context, [String? savedLocation]) async {
+  //Get http response
+  String message;
+  String cityName;
+  if (savedLocation != null) {
+    cityName = savedLocation;
+  } else {
+    cityName = locationTextController.text;
+  }
+  if (cityName.length > 1) {
+    var response = await http.get(Uri.parse(
+        "http://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=${API.apiKey}"));
+    message = response.body;
+    print('Status code : ${response.statusCode}');
+    if (response.statusCode == 200) {
+      if (savedLocation == null) {
+        saveLocation(cityName);
+      }
+      gotoCurrentWeather(context, message);
+    }
+  } else {
+    message = 'Please enter a city name';
+  }
 }
