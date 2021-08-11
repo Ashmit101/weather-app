5780279cfeb5d8a4392e26973af0bba3f086068d
Ashmit Rajaure <ashmit.rajaure@gmail.com>
2021-08-11 13:53:04 +0545

    Change the default location
    See: #20

--- lib/tools/sembast_db.dart
+++ lib/tools/sembast_db.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:geolocator/geolocator.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:sembast/sembast.dart';
