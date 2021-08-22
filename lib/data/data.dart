a6ee862cb2163f4f2caf0c3ff023f0ef0f7d9a06
Ashmit Rajaure <ashmit.rajaure@gmail.com>
2021-08-22 11:47:02 +0545

    Add field to get custom API.
    See: #8

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,6 +1,6 @@
 class Constants {
   // static final String apiKey = '0cd1d9bfbf9969a81fc5105bf1239b9d';
-  static final String apiKey = '68d3784d0f6da71f53a458fc66562917';
+  static const String apiKey = '68d3784d0f6da71f53a458fc66562917';
 
   //Common phrases
   static final String error = 'Error';
@@ -16,30 +16,47 @@ class Constants {
   static final String standard = 'standard';
 
   //Return required api calls
-  static Uri oneCallApi(double lat, double lon, {String unit = 'metric'}) {
+  static Uri oneCallApi(double lat, double lon,
+      {String unit = 'metric', String? api}) {
+    print('(Constants) API stored: $api');
+    if (api?.length == 0 || api == null) {
+      api = apiKey;
+    }
+    print('(Constants) API called : $api');
     var uri = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=minutely&appid=$apiKey&units=$unit');
+        'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=minutely&appid=$api&units=$unit');
     return uri;
   }
 
   //Get location name from the coordinates
-  static Uri getLocationFromCoord(double lat, double lon) {
+  static Uri getLocationFromCoord(double lat, double lon, {String? api}) {
+    if (api?.length == 0 || api == null) {
+      api = apiKey;
+    }
+    print('(Constants) API called : $api');
     var uri = Uri.parse(
-        'http://api.openweathermap.org/geo/1.0/reverse?lat=$lat&lon=$lon&appid=$apiKey');
+        'http://api.openweathermap.org/geo/1.0/reverse?lat=$lat&lon=$lon&appid=$api');
     return uri;
   }
 
   //Get coordinates from the cityname
-  static Uri getCoordFromLocation(String cityName) {
+  static Uri getCoordFromLocation(String cityName, {String? api}) {
+    if (api?.length == 0 || api == null) {
+      api = apiKey;
+    }
+    print('(Constants) API called : $api');
     var uri = Uri.parse(
-        'http://api.openweathermap.org/geo/1.0/direct?q=$cityName&limit=3&appid=$apiKey');
+        'http://api.openweathermap.org/geo/1.0/direct?q=$cityName&limit=3&appid=$api');
     return uri;
   }
 
   //Get the condition of current weather
-  static Uri getCurrentWeather(String cityName) {
+  static Uri getCurrentWeather(String cityName, {String? api}) {
+    if (api?.length == 0 || api == null) {
+      api = apiKey;
+    }
     var uri = Uri.parse(
-        'http://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey');
+        'http://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$api');
     return uri;
   }
 
--- lib/widgets/add_location.dart
+++ lib/widgets/add_location.dart
@@ -18,11 +18,19 @@ class AddLocation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController locationTextController = TextEditingController();
-    InputDecoration locationInputDecoration = InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Location',
-        hintText: 'Enter a name of a city');
+    TextEditingController textController = TextEditingController();
+    InputDecoration inputDecoration = InputDecoration();
+    if (_title == 'Change Location') {
+      inputDecoration = InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Location',
+          hintText: 'Enter a name of a city');
+    } else if (_title == 'Change API') {
+      inputDecoration = InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'API',
+          hintText: 'Enter your API key of OpenWeatherMap');
+    }
 
     return AlertDialog(
       title: Text(_title),
@@ -30,11 +38,17 @@ class AddLocation extends StatelessWidget {
         child: Column(
           children: [
             TextField(
-              controller: locationTextController,
-              decoration: locationInputDecoration,
+              controller: textController,
+              decoration: inputDecoration,
             ),
             ElevatedButton(
-              onPressed: () => submitLocation(context, locationTextController),
+              onPressed: () {
+                if (_title == 'Change Location') {
+                  submitLocation(context, textController);
+                } else if (_title == 'Change API') {
+                  submitAPI(context, textController);
+                }
+              },
               child: const Text('Submit'),
             ),
           ],
@@ -62,6 +76,15 @@ class AddLocation extends StatelessWidget {
         var result = await sembastDb.addLocation(chosenLocation);
         Navigator.pop(context, chosenLocation);
       }
+    }
+  }
+
+  void submitAPI(
+      BuildContext context, TextEditingController textController) async {
+    String api = textController.text;
+    int id = await sembastDb.addApiKey(api);
+    if (id >= 0) {
+      Navigator.pop(context);
     }
   }
 }
