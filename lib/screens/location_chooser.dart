22ff17310f593053f7d4be3d7cfe124fa88922ae
Ashmit Rajaure <ashmit.rajaure@gmail.com>
2021-08-24 12:08:24 +0545

    BugFix. Cancel changing location, show internet problem.

--- lib/screens/location_chooser.dart
+++ lib/screens/location_chooser.dart
@@ -5,6 +5,7 @@ import 'package:weather/screens/today_details.dart';
 import 'package:weather/tools/weather_downloader.dart';
 import 'package:weather/widgets/choose_coord.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import '../tools/sembast_db.dart';
 
 TextEditingController locationTextController = TextEditingController();
@@ -12,9 +13,6 @@ TextEditingController apiTextController = TextEditingController();
 SembastDb sembastDb = SembastDb();
 
 class LocationChooser extends StatefulWidget {
-  LocationChooser() {
-    print('location chooser constructor');
-  }
   @override
   _LocationChooserState createState() {
     return _LocationChooserState();
@@ -29,6 +27,17 @@ class _LocationChooserState extends State<LocationChooser> {
 
   var _isDeviceLocationProgressVisible = false;
 
+  Future<int> checkInternet() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile) {
+      return 0;
+    } else if (connectivityResult == ConnectivityResult.wifi) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     String message = 'Starting...';
@@ -36,6 +45,7 @@ class _LocationChooserState extends State<LocationChooser> {
         color: Colors.white, fontSize: 12, decoration: TextDecoration.none);
 
     // print('Location chooser building');
+    Future<int> internet = checkInternet();
     Future<List<GeoLocation>> savedLocations = getSavedLocation();
     final ButtonStyle buttonStyle =
         ElevatedButton.styleFrom(textStyle: TextStyle(fontSize: 24));
@@ -51,132 +61,164 @@ class _LocationChooserState extends State<LocationChooser> {
     );
 
     return FutureBuilder(
-        future: savedLocations,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            //After resolving the future
-            if (snapshot.hasData) {
-              var savedLocation = snapshot.data as List<GeoLocation>;
-              print('Object received');
-              if (savedLocation.isEmpty) {
-                return Scaffold(
-                    appBar: AppBar(
-                      title: Text('Location'),
-                    ),
-                    body: SingleChildScrollView(
-                      child: Container(
-                        padding: EdgeInsets.all(15.0),
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.explore,
-                              size: 50,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text("Search your location"),
-                            ),
-                            TextField(
-                              decoration: locationInputDecoration,
-                              controller: locationTextController,
-                            ),
-                            Container(
-                              height: 8,
-                            ),
-                            //API textfield
-                            TextField(
-                              decoration: apiInputDecoration,
-                              controller: apiTextController,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                ElevatedButton(
-                                  style: buttonStyle,
-                                  onPressed: () {
-                                    String apiKey = apiTextController.text;
-                                    String cityName =
-                                        locationTextController.text;
-                                    if (cityName != '') {
-                                      setState(() {
-                                        _isProgressVisible = true;
-                                      });
-                                      submit(context, apiKey, cityName);
-                                    }
-                                  },
-                                  child: Text('Submit'),
-                                ),
-                                Visibility(
-                                  visible: _isProgressVisible,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: SizedBox(
-                                        height: 15,
-                                        width: 15,
-                                        child: CircularProgressIndicator(
-                                          strokeWidth: 3,
-                                        )),
+      future: internet,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            var internetCode = snapshot.data as int;
+            if (internetCode == 1) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: const Text("No Internet"),
+                ),
+                body: Center(
+                  child: Image(
+                    image: AssetImage('graphics/weather_nointernet.png'),
+                  ),
+                ),
+              );
+            } else {
+              return FutureBuilder(
+                  future: savedLocations,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      //After resolving the future
+                      if (snapshot.hasData) {
+                        var savedLocation = snapshot.data as List<GeoLocation>;
+                        if (savedLocation.isEmpty) {
+                          return Scaffold(
+                              appBar: AppBar(
+                                title: Text('Location'),
+                              ),
+                              body: SingleChildScrollView(
+                                child: Container(
+                                  padding: EdgeInsets.all(15.0),
+                                  child: Column(
+                                    children: [
+                                      Icon(
+                                        Icons.explore,
+                                        size: 50,
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text("Search your location"),
+                                      ),
+                                      TextField(
+                                        decoration: locationInputDecoration,
+                                        controller: locationTextController,
+                                      ),
+                                      Container(
+                                        height: 8,
+                                      ),
+                                      //API textfield
+                                      TextField(
+                                        decoration: apiInputDecoration,
+                                        controller: apiTextController,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          ElevatedButton(
+                                            style: buttonStyle,
+                                            onPressed: () {
+                                              String apiKey =
+                                                  apiTextController.text;
+                                              String cityName =
+                                                  locationTextController.text;
+                                              if (cityName != '') {
+                                                setState(() {
+                                                  _isProgressVisible = true;
+                                                });
+                                                submit(
+                                                    context, apiKey, cityName);
+                                              }
+                                            },
+                                            child: Text('Submit'),
+                                          ),
+                                          Visibility(
+                                            visible: _isProgressVisible,
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: SizedBox(
+                                                  height: 15,
+                                                  width: 15,
+                                                  child:
+                                                      CircularProgressIndicator(
+                                                    strokeWidth: 3,
+                                                  )),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      ElevatedButton(
+                                        style: buttonStyle,
+                                        onPressed: () {
+                                          setState(() {
+                                            _isDeviceLocationProgressVisible =
+                                                true;
+                                          });
+                                          return getDeviceLocation(context);
+                                        },
+                                        child: Text('Use device location'),
+                                      ),
+                                      Visibility(
+                                        visible:
+                                            _isDeviceLocationProgressVisible,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: SizedBox(
+                                              height: 15,
+                                              width: 15,
+                                              child: CircularProgressIndicator(
+                                                strokeWidth: 3,
+                                              )),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                              ],
+                              ));
+                        }
+                        gotoCurrentWeather(context, savedLocation[0]);
+                      }
+                    }
+                    //While waiting
+                    return Scaffold(
+                      appBar: AppBar(
+                        title: Text('Weather'),
+                      ),
+                      body: Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: FlutterLogo(),
                             ),
-                            ElevatedButton(
-                              style: buttonStyle,
-                              onPressed: () {
-                                setState(() {
-                                  _isDeviceLocationProgressVisible = true;
-                                });
-                                return getDeviceLocation(context);
-                              },
-                              child: Text('Use device location'),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: CircularProgressIndicator(),
                             ),
-                            Visibility(
-                              visible: _isDeviceLocationProgressVisible,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: SizedBox(
-                                    height: 15,
-                                    width: 15,
-                                    child: CircularProgressIndicator(
-                                      strokeWidth: 3,
-                                    )),
-                              ),
-                            ),
+                            Text(
+                              message,
+                              style: messageStyle,
+                            )
                           ],
                         ),
                       ),
-                    ));
-              }
-              gotoCurrentWeather(context, savedLocation[0]);
+                    );
+                  });
             }
           }
-          //While waiting
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Weather'),
-            ),
-            body: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: FlutterLogo(),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: CircularProgressIndicator(),
-                  ),
-                  Text(
-                    message,
-                    style: messageStyle,
-                  )
-                ],
-              ),
-            ),
-          );
-        });
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 
   Future<List<GeoLocation>> getSavedLocation() async {
